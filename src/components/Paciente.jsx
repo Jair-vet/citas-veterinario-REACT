13f89a82571512231a7e3f5f90c62b54595f948d
Jair <jairaceves56@gmail.com>
2022-03-14 11:52:13 -0600

    Estructura base de la App

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,32 @@
+
+
+const Paciente = () => {
+  return (
+    <div className="m-4 bg-white shadow-md px-5 py-10 rounded-xl">
+             
+        <p className="font-bold mb-3 text-gray-800 uppercase">Nombre: {''}
+            <span className="font-normal normal-case">Hook</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-800 uppercase">Propietario: {''}
+            <span className="font-normal normal-case">Jair Aceves</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-800 uppercase">Email: {''}
+            <span className="font-normal normal-case">coreo@correo.com</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-800 uppercase">Fecha: {''}
+            <span className="font-normal normal-case">12/Enero/2022</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-800 uppercase">Sintomas: {''}
+            <span className="font-normal normal-case">Labore non id exercitation labore deserunt aliqua duis consectetur eiusmod. Dolore aliqua ullamco duis commodo ex do aliquip consectetur excepteur elit qui elit in. Occaecat aute nostrud aliquip aute id nisi occaecat anim aute commodo reprehenderit occaecat qui Lorem. Nulla voluptate amet culpa exercitation sint do dolore. Enim nisi qui dolor proident sit do ipsum. Commodo sint esse aute irure exercitation dolor do fugiat deserunt.
+                Do excepteur Lorem ad id reprehenderit ut est exercitation ullamco quis labore eiusmod. Velit qui magna aute deserunt eu occaecat nisi cillum ut sunt fugiat est. Eu do in magna excepteur anim aute irure id in incididunt laborum culpa qui. Velit minim ullamco esse occaecat. Eu laborum incididunt qui ea laboris ea cillum fugiat labore velit consectetur reprehenderit. Nisi mollit minim fugiat anim dolore.
+            </span>                  
+        </p>
+    </div>
+  )
+}
+
+export default Paciente
