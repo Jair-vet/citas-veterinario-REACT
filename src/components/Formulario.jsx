71094a432b79d425b292623ef0aa2a5cb8cbab77
Jair <jairaceves56@gmail.com>
2022-03-15 08:58:40 -0600

    Validacion de Campos llenos obligatoria

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -8,18 +8,21 @@ const Formulario = () => {
     const [fecha, setFecha] = useState('');
     const [sintomas, setSintomas] = useState('');
 
+    const [error, setError] = useState(false);
 
     const handleSubmit = (e) => {
+        e.preventDefault()
 
         // Validacion del Formulario
         if( [ nombre, propietario, email, fecha, sintomas ].includes('') ){
             console.log('Hay almenos un campo vacio');
-        }else{
-            console.log('Todos llenos');
+
+            setError(true)
+            return
         }
 
-        e.preventDefault()
-        console.log('Enviando Formulario');
+        setError(false) // Cambiamos el state
+
     }
 
 
@@ -38,6 +41,12 @@ const Formulario = () => {
                 onSubmit={handleSubmit}
                 className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
             >
+                {error && 
+                    <div className="bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+                        <p>Todos los campos son Obligatorios</p>
+                    </div>
+                    
+                }
 
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-800 uppercase 
