50b9e4e0aaedd01978450f81f6df0f2a98b42f4a
Jair <jairaceves56@gmail.com>
2022-03-19 10:38:52 -0600

    Creamos el props de Mensaje de Error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react';
-
+import Error from './Error';
 
 const Formulario = ({pacientes, setPacientes}) => {
     const [nombre, setNombre] = useState('');
@@ -60,12 +60,7 @@ const Formulario = ({pacientes, setPacientes}) => {
                 onSubmit={handleSubmit}
                 className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
             >
-                {error && 
-                    <div className="bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
-                        <p>Todos los campos son Obligatorios</p>
-                    </div>
-                    
-                }
+                {error && <Error mensaje='Todos los campos son Obligatorios'/> }
 
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-800 uppercase 
