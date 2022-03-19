ca523b3e1a75e9ca0eead9a6d0590ec293975155
Jair <jairaceves56@gmail.com>
2022-03-19 12:37:53 -0600

    Listado de Pacientes Terminado

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,35 +1,38 @@
 import Paciente from "./Paciente"
 
-const ListadoPacientes = ({pacientes, setPacientes}) => {
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
+    return (
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
 
-  return (
-      <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+            {pacientes && pacientes.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Administra tus {''}
+                        <span className="text-indigo-900 font-bold ">Pacientes y Citas</span>
+                    </p>
 
-          <h2 className="font-black text-3xl text-center">ListadoPacientes</h2>
-          <p className="text-xl mt-5 mb-10 text-center">
-              Administra tus {''}
-              <span className="text-indigo-900 font-bold">Pacientes y Citas</span>
-          </p>
+                    { pacientes.map( paciente => (
+                        <Paciente 
+                            key={paciente.id}
+                            paciente={paciente}
+                            setPaciente={setPaciente}
+                            eliminarPaciente={eliminarPaciente}
+                        />
+                    ))}
+                </>
 
-            {/* imprimir los valores en comsola usando .map() */}
-            {pacientes.map( paciente => {
-
-                return(
-                    // llama este componente tantas veces se vaya creando
-                    <Paciente 
-                         // Siempre que mandemos un arreglo debemos mandar un key Unico
-                         // Creamos un Id Unico
-                        key={paciente.id}
-                        paciente={paciente}
-                    />
-                )
-
-            })}
-
-         
-      
-      </div>
-  )
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Comienza agregando pacientes {''}
+                        <span className="text-indigo-900 font-bold ">y aparecerán en este lugar</span>
+                    </p>
+                </>
+            )}
+        </div>
+    )
 }
 
 export default ListadoPacientes
